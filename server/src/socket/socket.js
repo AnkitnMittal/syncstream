40f23857577de8de7d4d4cf62745dbf3a77ae1e1
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-07-29 09:56:41 +0000

    flatten codebase

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 import { configureRedisAdapter } from '../config/redisClient.js';
-import { registerSocketEvents } from './socketRouter.js';
+
+import { joinRoom, handleDisconnect, handleRequestInitialState } from './handlers/roomHandlers.js';
+import { updateLanguage, syncDocUpdate } from './handlers/docHandlers.js';
 
 export async function initSocket(httpServer) {
   const io = new Server(httpServer, {
@@ -14,7 +16,16 @@ export async function initSocket(httpServer) {
 
   await configureRedisAdapter(io);
 
-  registerSocketEvents(io);
+  io.on('connection', (socket) => {
+    console.log(`WebSocket Connected: ${socket.id}`);
+
+    socket.on('join_room', joinRoom);
+    socket.on('request_initial_state', handleRequestInitialState);
+    socket.on('disconnect', handleDisconnect);
+
+    socket.on('update_room_language', updateLanguage);
+    socket.on('sync_doc_update', syncDocUpdate);
+  });
 
   return io;
 }
